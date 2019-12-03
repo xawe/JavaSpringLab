ce9fe32e08dce4dffdaf4bc249639a179b8ecf1d
Xawe <dreamxawe@hotmail.com>
2019-12-03 19:55:10 -0300

    .

--- src/main/java/xawe/com/springlab/springlab/DemoApplication.java
+++ src/main/java/xawe/com/springlab/springlab/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestTemplate;
-
+//no....
 @SpringBootApplication
 public class DemoApplication {
 
