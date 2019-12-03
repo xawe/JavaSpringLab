51255e3920236b627b232fce982c60210a1a39e7
Xawe <dreamxawe@hotmail.com>
2019-12-03 12:33:14 -0300

    Initial commit

---
+++ src/main/java/xawe/com/springlab/springlab/controllers/ProductServiceController.java
@@ -0,0 +1,63 @@
+package xawe.com.springlab.springlab.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import xawe.com.springlab.springlab.model.*;
+
+@RestController
+public class ProductServiceController{
+    private static Map<String, Product> productRepo = new HashMap<>();
+
+    static{
+        Product honey = new Product();
+        honey.setid("1");
+        honey.setname("Mel de abelha");
+        productRepo.put(honey.getid(), honey);
+
+        Product almond = new Product();
+        almond.setid("2");
+        almond.setname("Almond");
+        productRepo.put(almond.getid(), almond);
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> delete(@PathVariable("id") String id){
+        productRepo.remove(id);
+        return new ResponseEntity<>("Produto removido com sucesso", HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/products/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product){
+        productRepo.remove(id);
+        product.setid(product.getid());
+        productRepo.put(product.getid(), product);
+        return new ResponseEntity<>("Produto atualizado com sucesso", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public ResponseEntity<Object> createProduct(@RequestBody Product product) {
+        productRepo.put(product.getid(), product);
+        return new ResponseEntity<>("Produto criado com sucesso", HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/products")
+    public ResponseEntity<Object> getProduct(){
+        return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/xawe/com/springlab/springlab/model/Product.java
@@ -0,0 +1,24 @@
+package xawe.com.springlab.springlab.model;
+
+public class Product{
+    private String id;
+    private String name;
+
+    public String getid(){
+        return id;
+    }
+    public void setid(String id){
+        this.id = id;
+    }
+
+    public String getname(){
+        return name;
+    }
+
+    public void setname(String name){
+        this.name = name;
+    }
+
+    
+
+}
